65d5a1305ba0ce3bf79975a06dbaa5c1999bdbe9
Jorge Eliecer <jorge.m.h@outlook.com>
2020-08-23 13:10:41 -0500

    strongly type the state module

--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -1,8 +1,44 @@
-import { createReducer, on, createAction } from '@ngrx/store';
+import { createReducer, on, createAction, createFeatureSelector, createSelector } from '@ngrx/store';
+import { Product } from './../product';
 
-export const productReducer = createReducer(
-  { showProductCode: true },
-  on(createAction('[Product] Toogle Product Code'), state => {
+import * as AppState from '../../state/app.state';
+
+export interface State extends AppState.State {
+  products: ProductState
+}
+
+export interface ProductState {
+  showProductCode: boolean;
+  currentProduct: Product;
+  products: Product[];
+}
+
+const initialState: ProductState = {
+  showProductCode: true,
+  currentProduct: null,
+  products: []
+};
+
+const getProductFeatureState = createFeatureSelector<ProductState>('products');
+
+export const getShowProductCode = createSelector(
+  getProductFeatureState,
+  state => state.showProductCode
+);
+
+export const getCurrentProduct = createSelector(
+  getProductFeatureState,
+  state => state.currentProduct
+);
+
+export const getProducts = createSelector(
+  getProductFeatureState,
+  state => state.products
+);
+
+export const productReducer = createReducer<ProductState>(
+  initialState,
+  on(createAction('[Product] Toogle Product Code'), (state): ProductState => {
     return {
       ...state,
       showProductCode: !state.showProductCode
